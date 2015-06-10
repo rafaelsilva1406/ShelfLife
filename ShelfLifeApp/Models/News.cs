bffcadcb384c0910c31e8e409240f2393d31ab5b
rafaelsilva1406 <rafaelsilva1406@gmail.com>
2015-06-10 16:28:14 -0700

    Model class files uploaded

---
+++ ShelfLifeApp/Models/News.cs
@@ -0,0 +1,11 @@
+﻿namespace ShelfLifeApp.Models
+{
+	using System;
+
+	public class News
+	{
+		public string Headline{ get; set;}
+		public string Story{ get; set;}
+	}
+}
+
---
+++ ShelfLifeApp/Models/User.cs
@@ -0,0 +1,16 @@
+﻿namespace ShelfLifeApp.Models
+{
+	using System;
+
+	public class User
+	{
+		public User ()
+		{
+		}
+
+		public string ID { get; set;}
+
+		public string Name { get; set; }
+	}
+}
+
