e3a6299e07ceb2d445ccbfcb930821f3d18250e1
SoyFood <matt.dye.ca@gmail.com>
2015-06-11 08:14:31 -0700

    Added BaseViewModel

---
+++ ShelfLifeApp/ViewModels/BaseViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ShelfLifeApp
+{
+	public abstract class BaseViewModel : INotifyPropertyChanged
+	{
+		#region INotifyPropertyChanged implementation
+
+		public event PropertyChangedEventHandler PropertyChanged = delegate {};
+
+		#endregion
+
+		protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			var handler = PropertyChanged;
+			if (handler != null)
+			{
+				handler(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+	}
+}
+
