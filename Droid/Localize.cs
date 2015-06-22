ad5430bec2b22a47a953ed4cc9a4b8a27c8b2334
rafaelsilva1406 <rafaelsilva1406@gmail.com>
2015-06-22 10:18:12 -0700

    Android Platform code to switch language

---
+++ Droid/Localize.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ShelfLifeApp;
+
+[assembly:Xamarin.Forms.Dependency(typeof(ShelfLifeApp.Droid.Localize))]
+namespace ShelfLifeApp.Droid
+{
+	public class Localize : ShelfLifeApp.ILocalize
+	{
+		public System.Globalization.CultureInfo GetCurrentCultureInfo ()
+		{
+			var androidLocale = Java.Util.Locale.Default;
+			var netLanguage = androidLocale.ToString().Replace ("_", "-"); // turns pt_BR into pt-BR
+			return new System.Globalization.CultureInfo(netLanguage);
+		}
+	}
+}
+
