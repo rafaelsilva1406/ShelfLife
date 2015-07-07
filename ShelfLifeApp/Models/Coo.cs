a4860dc0d146bfa3d31eddad734048997c4a365d
rafaelsilva1406 <rafaelsilva1406@gmail.com>
2015-07-07 01:28:23 -0700

    Added Class Model

---
+++ ShelfLifeApp/Models/Coo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ShelfLifeApp.Models
+{
+	public class Coo
+	{
+		public Coo (int id,String name)
+		{
+			ID = id;
+			Name = name;
+		}
+
+		public int ID { get; set;}
+		public String Name { get; set;}
+	}
+}
+
