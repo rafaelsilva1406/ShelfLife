3cf3d0989d301dbcf461d4b24296c3ed51bc5208
rafaelsilva1406 <rafaelsilva1406@gmail.com>
2015-07-07 11:06:25 -0700

    Added Class Model Actually Named the class plural Xamarin or .Net already use "size"

---
+++ ShelfLifeApp/Models/Size.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ShelfLifeApp.Models
+{
+	public class Sizes
+	{
+		public Sizes (int id,String name)
+		{
+			ID = id;
+			Name = name;
+		}
+
+		public int ID { get; set;}
+		public String Name { get; set;}
+	}
+}
+
