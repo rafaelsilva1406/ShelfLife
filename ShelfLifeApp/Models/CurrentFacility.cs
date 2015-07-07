25735b921b1076baf9eb9744a12c5b704ff24b66
rafaelsilva1406 <rafaelsilva1406@gmail.com>
2015-07-07 00:29:59 -0700

    Added Class Model

---
+++ ShelfLifeApp/Models/CurrentFacility.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ShelfLifeApp.Models
+{
+	public class CurrentFacility
+	{
+		public CurrentFacility (int id,String name)
+		{
+			ID = id;
+			Name = name;
+		}
+
+		public int ID { get; set;}
+
+		public String Name { get; set; }
+	}
+}
+
