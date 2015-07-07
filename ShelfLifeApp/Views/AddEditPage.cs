0eb2adf030710f8089cd34f557aef1e63f7b547a
rafaelsilva1406 <rafaelsilva1406@gmail.com>
2015-07-07 11:50:59 -0700

    Added Bindings on components Added Lists to Pickers

--- ShelfLifeApp/Views/AddEditPage.cs
+++ ShelfLifeApp/Views/AddEditPage.cs
@@ -5,11 +5,9 @@ using System.Collections;
 using System.Linq;
 using System.Resources;
 using System.Globalization;
-
 using Xamarin.Forms;
+using ShelfLifeApp.Models;
 using ShelfLifeApp.ViewModels;
-using ShelfLifeApp.Views;
-using ShelfLifeApp;
 
 namespace ShelfLifeApp.Views
 {
@@ -17,36 +15,39 @@ namespace ShelfLifeApp.Views
 	{
 		public StackLayout layout;
 		public UserDetailsViewModel userDetails;
+		public AddEditViewModel addEdit;
 		public ActivityIndicator loading;
-		public AddEditPage (UserDetailsViewModel userDetails)
+
+		public AddEditPage (UserDetailsViewModel userdetails)
 		{
-			this.userDetails = userDetails;
-			this.Title = AppResources.AddEditPageTitle;
-			this.loading = new ActivityIndicator ();
-			this.loading.IsRunning = true;
-			this.loading.IsEnabled = true;
-			this.loading.IsVisible = true;
-			this.layout = new StackLayout 
+			userDetails = userdetails;
+			addEdit = AddEditViewModel.Instance;
+			Title = AppResources.AddEditPageTitle;
+			loading = new ActivityIndicator ();
+			loading.IsRunning = true;
+			loading.IsEnabled = true;
+			loading.IsVisible = true;
+			layout = new StackLayout 
 			{
 				VerticalOptions = LayoutOptions.Center,
 				HorizontalOptions = LayoutOptions.CenterAndExpand,
 				Orientation = StackOrientation.Vertical,
 				BackgroundColor = Color.Transparent
 			};
-			this.layout.Children.Add (loading);
-			if(this.userDetails.isUserAuth == false){
-				this.Navigation.PopModalAsync();
-				this.Navigation.PushModalAsync (new LoginPage(this.userDetails));
+			layout.Children.Add(loading);
+
+			if(userDetails.isUserAuth == false){
+				Navigation.PopModalAsync();
+				Navigation.PushModalAsync (new LoginPage(userDetails));
 			}else{
-				this.layout.Children.Clear ();
+				layout.Children.Clear ();
 				init ();	
 			}
 		}
 
 		private void init()
 		{
-			this.BindingContext = this.userDetails;
-
+			BindingContext = addEdit;
 			var _grid1 = new Grid {
 				Padding = 4,
 				VerticalOptions = LayoutOptions.FillAndExpand,
@@ -76,12 +77,6 @@ namespace ShelfLifeApp.Views
 				BackgroundColor = Color.Transparent,
 				HeightRequest = 40
 			};
-			var _cooList = new List<String> ();
-			_cooList.Add ("CALI");
-			_cooList.Add ("MEX");
-			_cooList.Add ("PERU");
-			_cooList.Add ("CHILE");
-			_cooList.Add ("NZ");
 			Picker _picker1 = new Picker
 			{
 				Title = AppResources.AddEditPagePicker1,
@@ -91,12 +86,12 @@ namespace ShelfLifeApp.Views
 				BackgroundColor = Color.Transparent
 			};
 
-			foreach(string cooName in _cooList)
+			foreach(Coo coo in addEdit.GetDefaultCoo())
 			{
-				_picker1.Items.Add (cooName);
+				_picker1.Items.Add (coo.Name);
 			}
-				
-			var _packerList1 = new List<String> ();
+
+			_picker1.SetBinding (Picker.SelectedIndexProperty, "Coo");
 			Picker _picker2 = new Picker {
 				Title = AppResources.AddEditPagePicker2,
 				HeightRequest = 60,
@@ -112,10 +107,7 @@ namespace ShelfLifeApp.Views
 				IsVisible = false,
 				BackgroundColor = Color.Transparent
 			};
-			var _regionList = new List<String> ();
-			_regionList.Add ("North");
-			_regionList.Add ("Far North");
-			_regionList.Add ("South");
+			_entry1.SetBinding (Entry.TextProperty, "Grower");
 			Picker _picker3 = new Picker{ 
 				Title = AppResources.AddEditPagePicker3,
 				HeightRequest = 60,
@@ -124,12 +116,13 @@ namespace ShelfLifeApp.Views
 				IsVisible = false,
 				BackgroundColor = Color.Transparent
 			};
-
-			foreach(string regionName in _regionList)
+					
+			foreach(Region region in addEdit.GetDefaultRegion())
 			{
-				_picker3.Items.Add(regionName);
+				_picker3.Items.Add (region.Name);
 			}
 
+			_picker3.SetBinding (Picker.SelectedIndexProperty, "Region");
 			var _entry2 = new Entry{ 
 				Placeholder = AppResources.AddEditPageEntry2,
 				HeightRequest = 60,
@@ -137,51 +130,64 @@ namespace ShelfLifeApp.Views
 				IsVisible = false,
 				BackgroundColor = Color.Transparent
 			};
-
+			_entry2.SetBinding (Entry.TextProperty, "Pallet");
 			_picker1.SelectedIndexChanged += (sender, e) => {
-				_packerList1.Clear();
+				addEdit._PackerList.Clear();
 				_picker2.Items.Clear();
 				_entry1.IsVisible = false; 
 				_picker3.IsVisible = false;
 				_entry2.IsVisible = false;
+
 				switch(_picker1.SelectedIndex)
 				{
 					case 0:
-						_packerList1.Add("CD");
+						foreach(Packer packer in addEdit.GetDefaultPacker1())
+						{
+							_picker2.Items.Add (packer.Name);
+						}
+						
 						_entry1.IsVisible = true; 
 						_picker3.IsVisible = true;
 					break;
 					case 1:
-						_packerList1.Add("MX");
-						_packerList1.Add("MZ");
-						_packerList1.Add("MR");
-						_packerList1.Add("ML");
+						foreach(Packer packer in addEdit.GetDefaultPacker2())
+						{
+							_picker2.Items.Add (packer.Name);
+						}
+				
 						_entry2.IsVisible = true;
 					break;
 					case 2:
-						_packerList1.Add("Camposol");
-						_packerList1.Add("AvoPack");
+						foreach(Packer packer in addEdit.GetDefaultPacker3())
+						{
+							_picker2.Items.Add (packer.Name);
+						}
+
 						_entry2.IsVisible = true;
 					break;
 					case 3:
-						_packerList1.Add("TBA");
+						foreach(Packer packer in addEdit.GetDefaultPacker4())
+						{
+							_picker2.Items.Add (packer.Name);
+						}
+
 						_entry2.IsVisible = true;
 					break;
 					case 4:
-						_packerList1.Add("TBA");
+						foreach(Packer packer in addEdit.GetDefaultPacker5())
+						{
+							_picker2.Items.Add (packer.Name);
+						}
 						_entry2.IsVisible = true;
 					break;
 					default:
 						//throw exception
 					break;
 				}
-
-				foreach(string packerName in _packerList1)
-				{
-					_picker2.Items.Add(packerName);
-				}
+					
 				_picker2.IsVisible = true;
 			};
+			_picker2.SetBinding (Picker.SelectedIndexProperty, "Packer");
 			DatePicker _datePicker1 = new DatePicker
 			{
 				Format = "D",
@@ -189,10 +195,7 @@ namespace ShelfLifeApp.Views
 				HeightRequest = 60,
 				BackgroundColor = Color.Transparent
 			};
-			var _sizeList = new List<String> ();
-			_sizeList.Add ("Small");
-			_sizeList.Add ("Medium");
-			_sizeList.Add ("Large");
+			_datePicker1.SetBinding(DatePicker.DateProperty, new Binding("Date"));
 			Picker _picker4 = new Picker{ 
 				Title = AppResources.AddEditPagePicker4,
 				HeightRequest = 60,
@@ -201,11 +204,12 @@ namespace ShelfLifeApp.Views
 				BackgroundColor = Color.Transparent
 			};
 
-			foreach(string sizeName in _sizeList)
+			foreach(Sizes size in addEdit.GetDefaultSize())
 			{
-				_picker4.Items.Add(sizeName);
+				_picker4.Items.Add(size.Name);
 			}
 
+			_picker4.SetBinding (Picker.SelectedIndexProperty, "Size");
 			var _button1 = new Button {
 				Text = AppResources.AddEditPageButton1,
 				HeightRequest = 80,
@@ -220,7 +224,15 @@ namespace ShelfLifeApp.Views
 					WinPhone: "Comic Sans MS"
 				)
 			};
-
+			_button1.Clicked += (sender, e) => {
+				System.Diagnostics.Debug.WriteLine("Coo: {0}",addEdit.Coo);
+				System.Diagnostics.Debug.WriteLine("Grower: {0}",addEdit.Grower);
+				System.Diagnostics.Debug.WriteLine("Region: {0}",addEdit.Region);
+				System.Diagnostics.Debug.WriteLine("Packer: {0}",addEdit.Packer);
+				System.Diagnostics.Debug.WriteLine("Pallet: {0}",addEdit.Pallet);
+				System.Diagnostics.Debug.WriteLine("Date: {0}",addEdit.Date);
+				System.Diagnostics.Debug.WriteLine("Size: {0}",addEdit.Size);
+			};
 			_grid1.Children.Add (_searchBar1,0, 3, 0, 1);
 			_grid1.Children.Add (_picker1,0,1);
 			_grid1.Children.Add (_datePicker1,1,1);
@@ -230,7 +242,7 @@ namespace ShelfLifeApp.Views
 			_grid1.Children.Add (_picker3,1,3);
 			_grid1.Children.Add (_entry2,1,4);
 			_grid1.Children.Add (_button1,0,6,5,6);
-			this.Content = _grid1;
+			Content = _grid1;
 		}
 	}
 }
