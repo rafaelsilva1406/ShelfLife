f56bb957eea05c873e2c6cefb22135aa6fce1405
rafaelsilva1406 <rafaelsilva1406@gmail.com>
2015-07-07 10:26:43 -0700

    Added Class Model

---
+++ ShelfLifeApp/Models/Region.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ShelfLifeApp.Models
+{
+	public class Region
+	{
+		public Region (int id,String name)
+		{
+			ID = id;
+			Name = name;
+		}
+
+		public int ID { get; set;}
+		public String Name { get; set;}
+	}
+}
+
