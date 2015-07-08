01f6324121a37ac8d4e449e771d8982f9f340248
rafaelsilva1406 <rafaelsilva1406@gmail.com>
2015-07-07 23:38:59 -0700

    Added Base Class Web Services

---
+++ ShelfLifeApp/Services/BaseService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ShelfLifeApp.Services
+{
+	public class BaseService
+	{
+		public BaseService ()
+		{
+		}
+
+		public async Task<string> GetAsync(string uri)
+		{
+			var httpClient = new HttpClient ();
+			var response = await httpClient.GetAsync (uri);
+			response.EnsureSuccessStatusCode ();
+			string content = await response.Content.ReadAsStringAsync ();
+			return content;
+		}
+	}
+}
+
