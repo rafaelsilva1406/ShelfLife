fbbdd0f2317946a7481386d436bc163c9755c684
rafaelsilva1406 <rafaelsilva1406@gmail.com>
2015-07-07 23:36:30 -0700

    Added Class Model

---
+++ ShelfLifeApp/Models/Earthquake.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShelfLifeApp.Models
+{
+	public class Earthquake
+	{
+		public string eqid { get; set; }
+		public float magnitude { get; set; }
+		public float lng { get; set; }
+		public string src { get; set; }
+		public string datetime { get; set; }
+		public float depth { get; set; }
+		public float lat { get; set; }
+	}
+}
+
