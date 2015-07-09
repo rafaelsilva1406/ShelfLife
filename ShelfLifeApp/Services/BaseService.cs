7befe43509943a24aafb8ce02ede4a67c38da307
rafaelsilva1406 <rafaelsilva1406@gmail.com>
2015-07-08 22:07:35 -0700

    Added Post Http Headers Added Post Gzip Logic

--- ShelfLifeApp/Services/BaseService.cs
+++ ShelfLifeApp/Services/BaseService.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.IO;
+using System.IO.Compression;
 
 namespace ShelfLifeApp.Services
 {
@@ -13,6 +15,8 @@ namespace ShelfLifeApp.Services
 		public async Task<string> GetAsync(string uri)
 		{
 			var httpClient = new HttpClient ();
+			httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Accept-Encoding", "gzip, deflate");
+			httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Accept-Language", "en,en-US,es-PE;");
 			var response = await httpClient.GetAsync (uri);
 			response.EnsureSuccessStatusCode ();
 			string content = await response.Content.ReadAsStringAsync ();
@@ -22,10 +26,14 @@ namespace ShelfLifeApp.Services
 		public async Task<string> PostAsync(string uri, string data)
 		{
 			var httpClient = new HttpClient ();
+			httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Accept-Encoding", "gzip, deflate");
+			httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Accept-Language", "en,en-US,es-PE;");
 			var response = await httpClient.PostAsync (uri, new StringContent (data));
-			response.EnsureSuccessStatusCode ();
-			string content = await response.Content.ReadAsStringAsync ();
-			return content;
+			//response.EnsureSuccessStatusCode ();
+			var responseStream  = await response.Content.ReadAsStreamAsync ();
+			var decompressedStream = new GZipStream (responseStream , CompressionMode.Decompress);
+			var streamReader = new StreamReader (decompressedStream);
+			return streamReader.ReadToEnd();
 		}
 	}
 }
