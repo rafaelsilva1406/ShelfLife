e4e5e0ab2136428191e334475fc4fb0a5b220a8d
rafaelsilva1406 <rafaelsilva1406@gmail.com>
2015-07-13 13:15:56 -0700

    Added Custom Class Check Types

---
+++ ShelfLifeApp/Custom/TypeCheck.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+namespace ShelfLifeApp.Custom
+{
+	public class TypeCheck
+	{
+		public TypeCheck ()
+		{
+		}
+
+		public bool isValidJson(string json)
+		{
+			try {
+				JToken token = JObject.Parse(json);
+				return true; 
+			}
+			catch(Exception ex){ 
+				return false;
+			}
+		}
+	}
+}
+
