13d1dd711aae2885d27673d0147fcae5bc49ba1f
rafaelsilva1406 <rafaelsilva1406@gmail.com>
2015-07-14 10:56:30 -0700

    Added Custom Control Renderer

---
+++ Droid/MyEntryRenderer.cs
@@ -0,0 +1,28 @@
+﻿using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms;
+using ShelfLifeApp.Custom;
+using ShelfLifeApp.Droid;
+
+[assembly:ExportRenderer(typeof (MyEntry), typeof (MyEntryRenderer))]
+
+namespace ShelfLifeApp.Droid
+{
+	public class MyEntryRenderer : EntryRenderer
+	{
+		// Override the OnElementChanged method so we can tweak this renderer post-initial setup
+		protected override void OnElementChanged (ElementChangedEventArgs<Entry> e)
+		{
+			base.OnElementChanged (e);
+
+			if (Control != null) {
+				// do whatever you want to the textField here!
+				Control.SetBackgroundColor(global::Android.Graphics.Color.White);
+				Control.SetTextColor(global::Android.Graphics.Color.DarkGray);
+				Control.InputType = Android.Text.InputTypes.TextFlagNoSuggestions;
+				Control.SetPadding (20,5,5,5);
+				Control.SetHeight(120);
+			}
+		}
+	}
+}
+
---
+++ Droid/MyPickerRenderer.cs
@@ -0,0 +1,26 @@
+﻿using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms;
+using ShelfLifeApp.Custom;
+using ShelfLifeApp.Droid;
+
+[assembly:ExportRenderer(typeof(MyPicker),typeof(MyPickerRenderer))]
+
+namespace ShelfLifeApp.Droid
+{
+	public class MyPickerRenderer : PickerRenderer
+	{
+		protected override void OnElementChanged (ElementChangedEventArgs<Picker> e)
+		{
+			base.OnElementChanged (e);
+
+			if (e.OldElement == null){
+				Control.SetBackgroundColor (global::Android.Graphics.Color.White);
+				Control.SetTextColor (global::Android.Graphics.Color.DarkGray);
+				Control.InputType = Android.Text.InputTypes.TextFlagNoSuggestions;
+				Control.SetPadding (20,5,5,5);
+				Control.SetHeight (120);
+			}
+		}
+	}
+}
+
