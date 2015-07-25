b5672578b7dcce0ff367a48db81686ef4025d92b
rafaelsilva1406 <rafaelsilva1406@gmail.com>
2015-07-25 10:19:09 -0700

    Added custom renderer

---
+++ Droid/MyEntryPasswordRenderer.cs
@@ -0,0 +1,27 @@
+﻿using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms;
+using ShelfLifeApp.Custom;
+using ShelfLifeApp.Droid;
+
+[assembly:ExportRenderer(typeof(MyEntryPassword), typeof(MyEntryPasswordRenderer))]
+
+namespace ShelfLifeApp.Droid
+{
+	public class MyEntryPasswordRenderer : EntryRenderer
+	{
+		// Override the OnElementChanged method so we can tweak this renderer post-initial setup
+		protected override void OnElementChanged (ElementChangedEventArgs<Entry> e)
+		{
+			base.OnElementChanged (e);
+
+			if (Control != null) {
+				// do whatever you want to the textField here!
+				Control.SetBackgroundColor(global::Android.Graphics.Color.White);
+				Control.SetTextColor(global::Android.Graphics.Color.DarkGray);
+				Control.SetPadding (20,5,5,5);
+				Control.SetHeight(120);
+			}
+		}
+	}
+}
+
