cbe5e1ac4a8e484ccb1f49fdf2efda98f6bf36a0
rafaelsilva1406 <rafaelsilva1406@gmail.com>
2015-07-29 10:53:19 -0700

    Added custom renderers for label & search

---
+++ Droid/MyLabelRenderer.cs
@@ -0,0 +1,26 @@
+﻿using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms;
+using ShelfLifeApp.Custom;
+using ShelfLifeApp.Droid;
+
+[assembly:ExportRenderer(typeof(MyLabel), typeof(MyLabelRenderer))]
+
+namespace ShelfLifeApp.Droid
+{
+	public class MyLabelRenderer : LabelRenderer
+	{
+		// Override the OnElementChanged method so we can tweak this renderer post-initial setup
+		protected override void OnElementChanged (ElementChangedEventArgs<Label> e)
+		{
+			base.OnElementChanged (e);
+
+			if (Control != null) {
+				// do whatever you want to the textField here!
+				Control.SetBackgroundColor(global::Android.Graphics.Color.Transparent);
+				Control.SetTextColor(global::Android.Graphics.Color.LightGray);
+				Control.SetAllCaps (true);
+			}
+		}
+	}
+}
+
---
+++ Droid/MySearchBarRenderer.cs
@@ -0,0 +1,25 @@
+﻿using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms;
+using ShelfLifeApp.Custom;
+using ShelfLifeApp.Droid;
+
+[assembly:ExportRenderer(typeof(MySearchBar), typeof(MySearchBarRenderer))]
+
+namespace ShelfLifeApp.Droid
+{
+	public class MySearchBarRenderer : SearchBarRenderer
+	{
+		// Override the OnElementChanged method so we can tweak this renderer post-initial setup
+		protected override void OnElementChanged (ElementChangedEventArgs<SearchBar> e)
+		{
+			base.OnElementChanged (e);
+
+			if (Control != null) {
+				// do whatever you want to the textField here!
+				Control.SetBackgroundColor(global::Android.Graphics.Color.White);
+				Control.SetPadding (20,5,5,5);
+			}
+		}
+	}
+}
+
