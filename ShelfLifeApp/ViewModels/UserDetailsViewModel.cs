8500aeeb259e8301b7aba8f76f17f8aef5c54208
rafaelsilva1406 <rafaelsilva1406@gmail.com>
2015-07-30 14:59:01 -0700

    Removed unused code

--- ShelfLifeApp/ViewModels/UserDetailsViewModel.cs
+++ ShelfLifeApp/ViewModels/UserDetailsViewModel.cs
@@ -17,7 +17,6 @@ namespace ShelfLifeApp.ViewModels
 
 		private static readonly object padLock = new object ();
 		private static UserDetailsViewModel _Instance = null;
-		public ObservableCollection<FruitSample> _FruitList = new ObservableCollection<FruitSample>();
 		public List<CurrentFacility> _CurrentFacilityList = new List<CurrentFacility>();
 		private string _userName;
 		private string _userPassword;
@@ -37,7 +36,6 @@ namespace ShelfLifeApp.ViewModels
 						if(_Instance == null)
 						{
 							_Instance = new UserDetailsViewModel();
-							_Instance._FruitList = InitSamples ();
 						}
 					}
 				}
@@ -116,38 +114,6 @@ namespace ShelfLifeApp.ViewModels
 			_isUserAuth = false;
 			_domain = 0;
 			//_Domains.Clear ();
-		}
-
-		public int CaliCount{
-			get{
-				return _FruitList.Where (f => f.Origin.ID == 0).Count();
-			}
-		}
-		public int MexCount{
-			get{
-				return _FruitList.Where (f => f.Origin.ID == 1).Count ();
-			}
-		}
-		public int PeruCount{
-			get{
-				return _FruitList.Where (f => f.Origin.ID == 2).Count ();
-			}
-		}
-
-		private static ObservableCollection<FruitSample> InitSamples(){
-			var _VarFruitList = new  ObservableCollection<FruitSample>
-			{
-				new FruitSample(0, new CountryOfOrigin(){Description = "Cali", ID = 0}, "Camino del Sol", new DateTime(1975, 1, 15), 
-					"Large", 13, DateTime.Now),
-				new FruitSample(1, new CountryOfOrigin(){Description = "Mexico", ID = 1}, "Mission de Mexico", new DateTime(1975, 1, 15), 
-					"Small", 10, DateTime.Now),
-				new FruitSample(2, new CountryOfOrigin(){Description = "Peru", ID = 2}, "Camposol", new DateTime(1975, 1, 15), 
-					"Medium", 16, DateTime.Now),
-				new FruitSample(3, new CountryOfOrigin(){Description = "Cali", ID = 0}, "Camino del Sol", new DateTime(2015, 1, 15), 
-					"Medium", 16, DateTime.Now.AddDays(-1)),
-
-			};
-			return _VarFruitList;
 		}
 			
 		public List<CurrentFacility> GetDefaultCurrentFacilities()
