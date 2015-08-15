23b35919523efc969733caa3d9f21292607a1ff6
rafaelsilva1406 <rafaelsilva1406@gmail.com>
2015-08-15 09:15:07 -0700

    Added class model

---
+++ ShelfLifeApp/Models/Stage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ShelfLifeApp.Models
+{
+	public class Stage
+	{
+		public Stage (int id, string description)
+		{
+			ID = id;
+			Description = description;
+		}
+
+		public int ID{ get; set;}
+		public String Description{ get; set;}
+	}
+}
+
