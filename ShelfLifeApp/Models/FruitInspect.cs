145fd09f492584d29c4a0f998999ba382eba6691
rafaelsilva1406 <rafaelsilva1406@gmail.com>
2015-08-15 07:48:45 -0700

    Model for inspection

---
+++ ShelfLifeApp/Models/FruitInspect.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ShelfLifeApp.Models
+{
+	public class FruitInspect
+	{
+		public FruitInspect (string color,string stage,string lenticel,string defects,string cut,string comment)
+		{
+			Color = color;
+			Stage = stage;
+			Lenticel = lenticel;
+			Defects = defects;
+			Cut = cut;
+			Comment = comment;
+		}
+
+		public string Color {get; set;}
+
+		public string Stage { get; set;}
+
+		public string Lenticel { get; set;}
+
+		public string Defects { get; set;}
+
+		public string Cut { get; set;}
+
+		public string Comment{ get; set;}
+	}
+}
+
