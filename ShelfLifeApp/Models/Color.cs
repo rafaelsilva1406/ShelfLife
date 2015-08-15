f332b76367156471227c262810ad58e3fde02ad9
rafaelsilva1406 <rafaelsilva1406@gmail.com>
2015-08-15 08:37:33 -0700

    Added model for color dropdown

---
+++ ShelfLifeApp/Models/Color.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ShelfLifeApp.Models
+{
+	public class Color
+	{
+		public Color (int id, string description)
+		{
+			ID = id;
+			Description = description;
+		}
+
+		public int ID{get; set;}
+		public String Description{ get; set;}
+	}
+}
+
