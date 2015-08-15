fecf9cc6f7d44a12f6d314da81a7f7d01cde12ca
rafaelsilva1406 <rafaelsilva1406@gmail.com>
2015-08-15 09:30:58 -0700

    Added model class

---
+++ ShelfLifeApp/Models/Defect.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ShelfLifeApp.Models
+{
+	public class Defect
+	{
+		public Defect (int id, string desciption)
+		{
+			ID = id;
+			Description = desciption;
+		}
+
+		public int ID{ get; set;}
+		public String Description{ get; set;}
+	}
+}
+
