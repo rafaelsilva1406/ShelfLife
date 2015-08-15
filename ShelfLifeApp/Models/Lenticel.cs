ad4b46b145054faf2e91844414bd393b26698ffd
rafaelsilva1406 <rafaelsilva1406@gmail.com>
2015-08-15 09:14:47 -0700

    Added class model

---
+++ ShelfLifeApp/Models/Lenticel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ShelfLifeApp.Models
+{
+	public class Lenticel
+	{
+		public Lenticel (int id, string description)
+		{
+			ID = id;
+			Description = description;
+		}
+
+		public int ID{ get; set;}
+		public String Description{ get; set;}
+	}
+}
+
