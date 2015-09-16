24c491fd61a7fac6f37c0bd73674c5f318249a70
rafaelsilva1406 <rafaelsilva1406@gmail.com>
2015-09-16 11:19:17 -0700

    Modified UI layout

--- Droid/MySuccessButtonRenderer.cs
+++ Droid/MySuccessButtonRenderer.cs
@@ -14,19 +14,19 @@ namespace ShelfLifeApp.Droid
 			base.OnElementChanged (e);
 
 			if (e.OldElement == null){
-				Control.SetHeight (120);
+				Control.SetHeight (150);
 				Control.SetTextColor (global::Android.Graphics.Color.DarkGray);
 				var newBtn = (MySuccessButton)e.NewElement;
 				newBtn.SizeChanged += (s, args) => {
-					var radius = (float)System.Math.Min(10,10);	
+					var radius = (float)System.Math.Min(15,15);	
 					var normal = new global::Android.Graphics.Drawables.GradientDrawable ();
 					normal.SetColor(global::Android.Graphics.Color.LimeGreen);
 					normal.SetCornerRadius(radius);
-					normal.SetStroke(4,global::Android.Graphics.Color.LimeGreen);
+					normal.SetStroke(4,global::Android.Graphics.Color.LightGray);
 					var pressed = new global::Android.Graphics.Drawables.GradientDrawable ();
-					pressed.SetColor(global::Android.Graphics.Color.White);
+					pressed.SetColor(global::Android.Graphics.Color.DarkGoldenrod);
 					pressed.SetCornerRadius(radius);
-					pressed.SetStroke(4,global::Android.Graphics.Color.LimeGreen);
+					pressed.SetStroke(4,global::Android.Graphics.Color.LightGray);
 					var sld = new global::Android.Graphics.Drawables.StateListDrawable();
 					sld.AddState(new int[]{global::Android.Resource.Attribute.StatePressed},pressed);
 					sld.AddState(new int[]{},normal);
