a054326884c310f72f3867ab9db5d5badef515c2
rafael silva <rafaelsilva1406@gmail.com>
2015-11-10 11:10:33 -0800

    Added configurations to enable xlabs libs

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -7,6 +7,9 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using XLabs.Ioc;
+using XLabs.Platform.Device;
+using XLabs.Platform.Services;
 
 namespace ShelfLifeApp.Droid
 {
@@ -18,10 +21,15 @@ namespace ShelfLifeApp.Droid
 		{
 			base.OnCreate (bundle);
 
-			global::Xamarin.Forms.Forms.Init (this, bundle);
+			var container = new SimpleContainer ();
+			container.Register<IDevice> (t => AndroidDevice.CurrentDevice);
+			container.Register<IDisplay> (t => t.Resolve<IDevice> ().Display);
+			container.Register<INetwork>(t=> t.Resolve<IDevice>().Network);
 
+			Resolver.SetResolver (container.GetResolver ());
+
+			global::Xamarin.Forms.Forms.Init (this, bundle);
 			LoadApplication (new App ());
-		
 			ActionBar.SetHomeButtonEnabled (true);
 		}
 
